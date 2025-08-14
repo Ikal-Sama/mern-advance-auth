9433756b72e55bd2bcc0a0350515bc925455f573
Daniel Jhon <115909645+Ikal-Sama@users.noreply.github.com>
2025-08-14 16:29:52 +0800

    project completed

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import userRoutes from "./routes/user.route.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
@@ -31,5 +31,5 @@ if (process.env.NODE_ENV === "production") {
 
 app.listen(PORT, () => {
   connectToDB();
-  console.log("Server is running on port:", PORT);
+  console.log("Server is running on port", PORT);
 });
