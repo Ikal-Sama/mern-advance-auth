0a9ab76d7d53ed20737d76ba0e1b7a017fed0c20
Daniel Jhon <115909645+Ikal-Sama@users.noreply.github.com>
2025-08-14 17:11:11 +0800

    URL updated in authStore.js 2

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import axios from "axios";
 
 const API_URL =
-  import.meta.emv.MODE === "development"
+  import.meta.env.MODE === "development"
     ? `${import.meta.env.VITE_API_URL}/api`
     : "/api";
 
